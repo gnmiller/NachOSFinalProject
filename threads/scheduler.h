3291a518ba27c8d9f6ce66b6ce592b79fd7ad782
Can Li <tonylic1989@gmail.com>
2015-03-15 21:39:05 -0400

    Add threads and machine

---
+++ threads/scheduler.h
@@ -0,0 +1,54 @@
+// scheduler.h 
+//	Data structures for the thread dispatcher and scheduler.
+//	Primarily, the list of threads that are ready to run.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "copyright.h"
+#include "list.h"
+#include "thread.h"
+
+#ifdef CHANGED
+#include <queue>
+
+
+// The following class defines the scheduler/dispatcher abstraction -- 
+// the data structures and operations needed to keep track of which 
+// thread is running, and which threads are ready but not running.
+
+class MyCmp//functor, for priority queue
+{
+  public:
+	bool operator()(Thread* t1, Thread* t2)
+	{
+		return t1->getPriorityLevel() > t2->getPriorityLevel(); //the smallest priority level would be at the front of queue
+	}
+};
+#endif
+
+class Scheduler {
+  public:
+    Scheduler();			// Initialize list of ready threads 
+    ~Scheduler();			// De-allocate ready list
+
+    void ReadyToRun(Thread* thread);	// Thread can be dispatched.
+    Thread* FindNextToRun();		// Dequeue first thread on the ready 
+					// list, if any, and return thread.
+    void Run(Thread* nextThread);	// Cause nextThread to start running
+    void Print();			// Print contents of ready list
+    
+  private:
+#ifdef CHANGED
+    std::priority_queue<Thread*, std::vector<Thread*>, MyCmp> *readyList; //for thread which has priority level
+#else
+    List *readyList;  		// queue of threads that are ready to run,
+				// but not running
+#endif
+};
+
+#endif // SCHEDULER_H
